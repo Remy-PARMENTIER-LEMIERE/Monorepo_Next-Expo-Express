f0638767d6d5d7ebceb9874b3da4b14bd4badc37
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2026-02-05 11:32:47 +0100

    Settings seem to be done. Need to test the feature now.

---
+++ apps/mobile/lib/auth-client.ts
@@ -0,0 +1,13 @@
+import { expoClient } from "@better-auth/expo/client";
+import { createAuthClient } from "better-auth/react";
+import * as SecureStore from "expo-secure-store";
+export const authClient = createAuthClient({
+	baseURL: process.env.BASE_API_URL, // Base URL of your Better Auth backend.
+	plugins: [
+		expoClient({
+			scheme: process.env.EXPO_PUBLIC_APP_NAME,
+			storagePrefix: process.env.EXPO_PUBLIC_APP_NAME,
+			storage: SecureStore,
+		}),
+	],
+});
